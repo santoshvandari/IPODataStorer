e70ce42430597f75ff66be65c7e29017ba1b0760
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-03 21:59:30 +0545

    Initial

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
 
-os.system('pip install -r requirements.txt')
-print("Installing Playwright and Scrapy-Playwright")
+# os.system('pip install -r requirements.txt')
+os.system("python -m pip install --upgrade pip")
+print("Installing Playwright and Scrapy-Playwright packages")
 os.system('pip install scrapy-playwright')
+print("Installing Playwright")
 os.system('playwright install')
 # os.system('scrapy crawl ipo')
