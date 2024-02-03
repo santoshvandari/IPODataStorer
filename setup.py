fab2b3305905251489837abdec30aaeecc6d5cf3
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-03 22:07:12 +0545

    Initial

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import os
 
 os.system("python -m pip install --upgrade pip")
-os.system('pip install -r requirements.txt')
+# os.system('pip install -r requirements.txt')
+# psycopg2-binary
+# Scrapy
+# scrapy-playwright
+
+print("Installing Dependencies")
+os.system('pip install psycopg2-binary')
+os.system('pip install Scrapy')
 print("Installing Playwright and Scrapy-Playwright packages")
 os.system('pip install scrapy-playwright')
 print("Installing Playwright")
