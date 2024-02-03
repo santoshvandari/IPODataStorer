fbdcef06bb72932a29d35caab6f82242284bbefd
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-03 23:09:00 +0545

    Updated

--- IPODetails/settings.py
+++ IPODetails/settings.py
@@ -30,6 +30,7 @@ DOWNLOAD_DELAY = 2  # 2 seconds of delay
 
 
 
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 
 
 
@@ -41,7 +42,7 @@ DOWNLOAD_DELAY = 2  # 2 seconds of delay
 #USER_AGENT = 'IPODetails (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 # ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
