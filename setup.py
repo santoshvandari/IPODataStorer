263ddc49e424e5ea5990bd6386bdc287688e7596
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-02-03 22:10:28 +0545

    Initial

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import os
 
-os.system("python -m pip install --upgrade pip")
+os.system("sudo -H python -m pip install --upgrade pip")
 # os.system('pip install -r requirements.txt')
 # psycopg2-binary
 # Scrapy
 # scrapy-playwright
 
 print("Installing Dependencies")
-os.system('pip install psycopg2-binary')
-os.system('pip install Scrapy')
+os.system('sudo -H pip install psycopg2-binary')
+os.system('sudo -H pip install Scrapy')
 print("Installing Playwright and Scrapy-Playwright packages")
-os.system('pip install scrapy-playwright')
+os.system('sudo -H pip install scrapy-playwright')
 print("Installing Playwright")
-os.system('playwright install')
+os.system('sudo -H playwright install')
 os.system('scrapy crawl ipo')
